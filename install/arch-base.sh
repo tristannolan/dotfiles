48b87480b2ad93e271afdf45d8353c6564024e61
Tristan Nolan <tristannolan@users.noreply.github.com>
2026-02-05 21:48:47 +0200

    Ready to test partitioning

--- install/arch-base.sh
+++ install/arch-base.sh
@@ -11,11 +11,24 @@ set -euo pipefail
 #  SETTINGS  #
 ##############
 mode=safe
+unsafe=false
+
+memory_real_size_mb=$(awk '/MemTotal/ {print int($2/1024)}' /proc/meminfo)
+drive_real_size_mb=$(lsblk -nbd -o SIZE /dev/sda | awk '{printf "%.0f\n", $1/1024/1024}')
+
+drive_format_efi=ef00
+drive_format_linux_swap=8200
+drive_format_linux_filesystem=8300
 
 keyboard_layout=us
 network_available=false
 boot_mode=""
 drive=""
+partition_format_boot=$drive_format_efi
+partition_format_swap=$drive_format_linux_swap
+partition_format_system=$drive_format_linux_filesystem
+partition_size_boot=512M
+partition_size_swap=""
 
 ###############
 #  ARGUMENTS  #
@@ -23,14 +36,19 @@ drive=""
 
 usage() {
 	echo "Usage: $0 [options...]"
-	echo "-m	safe|dry|live"
+	echo "-u, --unsafe"
+	echo "-m, --mode	safe|dry|live"
 }
 
-while getopts ":hm:" opt; do
+while getopts ":hum:" opt; do
 	case "${opt}" in
 		h)
 			usage
 			exit 0
+			;;
+		u)
+			unsafe="true"
+			echo "UNSAFE EXECUTION - ABORT WILL BE IGNORED"
 			;;
 		m)
 			case "${OPTARG}" in
@@ -60,15 +78,21 @@ shift $((OPTIND - 1))
 abort() {
 	local reason=${1:-}
 	local info=${2:-}
+	local title="\nABORTING"
 
 	if [ -z "$reason" ]; then
-		echo "Aborting"
+		echo "$title"
 		exit 1
 	fi
 
-	echo -e "Aborting - $reason"
+	echo -e "$title - $reason" >&2
 	if [ -n "$info" ]; then
-		echo -e "$info"
+		echo -e "$info" >&2
+	fi
+
+	if [ $unsafe = "true" ]; then
+		echo -e "\nUNSAFE - IGNORING ABORT"
+		return
 	fi
 	exit 1
 }
@@ -80,11 +104,20 @@ confirm() {
 		abort "confirm()" "No question string provided"
 	fi
 
-	read -r -p "$question [y/N]: " confirm
-	case "$confirm" in
+	read -r -p "$question [y/N]: " answer
+	case "$answer" in
 		y|Y|yes|YES) echo "true" ;;
 		*) echo "false" ;;
 	esac
+}
+
+run_if_live() {
+	if [ "$mode" != "live" ]; then
+		echo -e "\nABORT - Last fallback before accidental command execution"
+		echo "Please urgently review safety features in script"
+		exit 1
+	fi
+	"$@"
 }
 
 #########
@@ -106,7 +139,28 @@ else
 	abort "Network unavailable" "Please review device and installer config"
 fi
 
+# Swap partition size
+max_swap=$(($drive_real_size_mb * 10 / 100))
+
+# What's the goal?
+# No more than 2x memory
+# No more than 10% drive space
+
+if (( memory_real_size_mb * 2 < max_swap)); then
+	partition_size_swap="$((memory_real_size_mb * 2))M"
+else
+	partition_size_swap="${max_swap}M"
+fi
+
+echo
+echo "Real Memory Size:	${memory_real_size_mb}M"
+echo "Real Drive Size:	${drive_real_size_mb}M"
+echo
+echo "Partition Swap Size:	$partition_size_swap"
+echo "Partition Boot Size:	$partition_size_boot"
+
 # Boot Mode
+echo
 if [ -d /sys/firmware/efi ]; then
 	read -r fw_size < /sys/firmware/efi/fw_platform_size
 	case "$fw_size" in
@@ -138,7 +192,7 @@ done
 
 if [[ "${#safe_drives[@]}" -eq 0 ]]; then
 	lsblk_output=$(lsblk)
-	abort "Select Drive" "No mountable drives available. Please confirm that an unused drive is available for partitioning. \n${lsblk_output}"
+	abort "No drive available" "Please confirm if an unmounted drive is available for partitioning. \n\n${lsblk_output}"
 fi
 
 while [ -z "$drive" ]; do
@@ -159,35 +213,36 @@ while [ -z "$drive" ]; do
 		continue
 	fi
 
-	#echo "You have selected drive ${drive_num}: ${drive}"
-	if [[ $(confirm "Proceed with ${drive_num}: /dev/${drive}?") == "false" ]]; then
+	drive="/dev/${safe_drives[${drive_num}]}"
+	if [[ $(confirm "Proceed with ${drive_num}: ${drive}?") == "false" ]]; then
+		drive=""
 		continue
 	fi
-
-	drive="${safe_drives[${drive_num}]}"
 done
+
+
+if [ "$mode" = "dry" ]; then
+	exit 0
+fi
 
 ##########
 #  LIVE  #
 ##########
 
-if [ "$mode" = "dry" ]; then
-	echo -e "\nExiting Dry Run - No commands have been run"
-	exit 0
-fi
-
 if [[ $(confirm "Proceed with live installation?") == "false" ]]; then
 	abort "User aborted"
 fi
 
-loadkeys $keyboard_layout
-timedatectl set-ntp true
-lsblk
+run_if_live loadkeys "$keyboard_layout"
+run_if_live timedatectl set-ntp true
 
-# create partitions here
-#
-# Find the correct drive
-# lsblk	
-#
-# Begin partitioning
-# parted /dev/vda
+# Partition
+run_if_live sgdisk --zap-all "${drive}"
+run_if_live sgdisk \
+	-n 1:0:+"$partition_size_boot}"		-t 1:"${partition_format_boot}" \
+	-n 2:0:+"${partition_size_swap}"	-t 2:"${partition_format_swap}" \
+	-n 3:0:0							-t 3:"${partition_format_system}" \
+	"${drive}"
+
+partprobe "$drive"
+lsblk "$drive"
